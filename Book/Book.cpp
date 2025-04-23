89f007b8e7f9ce1065892a39f254239e96a0f3a9
Lmg15186 <15186@ait.nsw.edu.au>
2025-04-23 20:15:15 +1000

    Update Book.cpp

--- Book/Book.cpp
+++ Book/Book.cpp
@@ -1,1 +1,16 @@
+// book.cpp
+#include "book.h"
+#include <iostream>
 
+Book::Book(string t, string a, string i, bool av, string d)
+    : title(t), author(a), ISBN(i), available(av), dateAdded(d) {}
+
+void Book::display() {
+    cout << "Title: " << title << "\nAuthor: " << author
+         << "\nISBN: " << ISBN << "\nAvailable: " << (available ? "Yes" : "No")
+         << "\nDate Added: " << dateAdded << endl;
+}
+
+string Book::getTitle() const {
+    return title;
+}
