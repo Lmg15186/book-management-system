75b2c6d5f5ef57d562f7e8f2a2f2435e70a78e3e
Lmg15186 <15186@ait.nsw.edu.au>
2025-04-23 20:18:03 +1000

    Update HardCopy.cpp

--- HardCopy/HardCopy.cpp
+++ HardCopy/HardCopy.cpp
@@ -1,1 +1,12 @@
+// hardcopy.cpp
+#include "hardcopy.h"
+#include <iostream>
+
+Hardcopy::Hardcopy(string t, string a, string i, bool av, string d, int p)
+    : Book(t, a, i, av, d), pages(p) {}
+
+void Hardcopy::display() {
+    Book::display();
+    cout << "Pages: " << pages << endl;
+}
 
