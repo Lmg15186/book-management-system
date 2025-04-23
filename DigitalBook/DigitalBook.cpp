d43e0ac8996fb37ed6bb3a042b815eedeae10bbb
Lmg15186 <15186@ait.nsw.edu.au>
2025-04-23 20:16:49 +1000

    Update DigitalBook.cpp

--- DigitalBook/DigitalBook.cpp
+++ DigitalBook/DigitalBook.cpp
@@ -1,1 +1,12 @@
+// digitalbook.cpp
+#include "digitalbook.h"
+#include <iostream>
+
+DigitalBook::DigitalBook(string t, string a, string i, bool av, string d, double f)
+    : Book(t, a, i, av, d), fileSize(f) {}
+
+void DigitalBook::display() {
+    Book::display();
+    cout << "File Size: " << fileSize << "MB" << endl;
+}
 
