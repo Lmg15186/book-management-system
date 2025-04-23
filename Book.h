1031f16c6b0ccf744b1d538a42b72ec9dad6869a
Lmg15186 <15186@ait.nsw.edu.au>
2025-04-23 20:14:21 +1000

    Update Book.h

--- Book.h
+++ Book.h
@@ -1,1 +1,22 @@
+// book.h
+#ifndef BOOK_H
+#define BOOK_H
 
+#include <string>
+using namespace std;
+
+class Book {
+protected:
+    string title;
+    string author;
+    string ISBN;
+    bool available;
+    string dateAdded;
+
+public:
+    Book(string t, string a, string i, bool av, string d);
+    virtual void display();
+    string getTitle() const;
+};
+
+#endif
