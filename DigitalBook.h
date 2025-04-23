f4d0d76d0d38127d1b7292741ee34ee04c3eaa64
Lmg15186 <15186@ait.nsw.edu.au>
2025-04-23 20:15:58 +1000

    Update DigitalBook.h

--- DigitalBook.h
+++ DigitalBook.h
@@ -1,1 +1,16 @@
+// digitalbook.h
+#ifndef DIGITALBOOK_H
+#define DIGITALBOOK_H
 
+#include "book.h"
+
+class DigitalBook : public Book {
+private:
+    double fileSize;
+
+public:
+    DigitalBook(string t, string a, string i, bool av, string d, double f);
+    void display() override;
+};
+
+#endif
