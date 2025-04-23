d3d7c6c85be6a5ca6b9700de101d47d50e7eb5d0
Lmg15186 <15186@ait.nsw.edu.au>
2025-04-23 20:17:24 +1000

    Update HardCopy.h

--- HardCopy.h
+++ HardCopy.h
@@ -1,1 +1,16 @@
+// hardcopy.h
+#ifndef HARDCOPY_H
+#define HARDCOPY_H
 
+#include "book.h"
+
+class Hardcopy : public Book {
+private:
+    int pages;
+
+public:
+    Hardcopy(string t, string a, string i, bool av, string d, int p);
+    void display() override;
+};
+
+#endif
