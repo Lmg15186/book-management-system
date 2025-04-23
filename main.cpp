b4393e29b34cead13248208d4c0871fa09e5d9a7
Lmg15186 <15186@ait.nsw.edu.au>
2025-04-23 15:44:56 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+Book arrayAsc[3] = {
+    Book("A Tale of Two Cities", "Charles Dickens", "001", true, "2023-01-01"),
+    Book("Moby Dick", "Herman Melville", "002", true, "2023-01-02"),
+    Book("War and Peace", "Leo Tolstoy", "003", true, "2023-01-03")
+};
+
+Book arrayDesc[3] = {
+    Book("War and Peace", "Leo Tolstoy", "003", true, "2023-01-03"),
+    Book("Moby Dick", "Herman Melville", "002", true, "2023-01-02"),
+    Book("A Tale of Two Cities", "Charles Dickens", "001", true, "2023-01-01")
+};
+
+Book arrayMixed[3] = {
+    Book("Moby Dick", "Herman Melville", "002", true, "2023-01-02"),
+    Book("A Tale of Two Cities", "Charles Dickens", "001", true, "2023-01-01"),
+    Book("War and Peace", "Leo Tolstoy", "003", true, "2023-01-03")
+};
 
