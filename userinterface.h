2ba760b428aadd7436026fa8be38c61046a130ee
Lmg15186 <15186@ait.nsw.edu.au>
2025-04-24 13:01:54 +1000

    Update userinterface.h

--- userinterface.h
+++ userinterface.h
@@ -1,1 +1,17 @@
+#ifndef USERINTERFACE_H
+#define USERINTERFACE_H
+#include "Book.h"
+#include <vector>
+#include <algorithm>
+
+class UserInterface {
+public:
+    static void sortBooksByTitle(vector<Book*>& books) {
+        sort(books.begin(), books.end(), [](Book* a, Book* b) {
+            return a->getTitle() < b->getTitle();
+        });
+    }
+};
+
+#endif
 
