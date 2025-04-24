4e917aedb5b311abc84628aa45e761cd6a5cbddb
Lmg15186 <15186@ait.nsw.edu.au>
2025-04-24 13:01:33 +1000

    Update HardCopy.h

--- HardCopy.h
+++ HardCopy.h
@@ -1,16 +1,16 @@
-// hardcopy.h
 #ifndef HARDCOPY_H
 #define HARDCOPY_H
-
-#include "book.h"
+#include "Book.h"
 
 class Hardcopy : public Book {
-private:
-    int pages;
-
 public:
-    Hardcopy(string t, string a, string i, bool av, string d, int p);
-    void display() override;
+    Hardcopy(string t, string a, string i, bool avail, string d)
+        : Book(t, a, i, avail, d) {}
+
+    void displayBookDetails() override {
+        cout << "[Hardcopy] ";
+        Book::displayBookDetails();
+    }
 };
 
 #endif
